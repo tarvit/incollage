c4f6be0cec877a8495ae173f031fc67d771deaa8
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-09-24 20:47:18 +0300

    RF: extract instagram connector to a separate file

---
+++ app/controllers/auth/instagram_connector.rb
@@ -0,0 +1,19 @@
+class AuthController
+
+  class InstagramConnector
+    attr_reader :redirect_uri
+
+    def initialize redirect_uri
+      @redirect_uri = redirect_uri
+    end
+
+    def authorize_url
+      Instagram.authorize_url(:redirect_uri => redirect_uri)
+    end
+
+    def get_access_token(code)
+      Instagram.get_access_token(code, :redirect_uri => redirect_uri).access_token
+    end
+  end
+
+end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,21 +1,5 @@
 class AuthController < ApplicationController
-
-  class InstagramConnector
-    attr_reader :redirect_uri
-
-    def initialize redirect_uri
-      @redirect_uri = redirect_uri
-    end
-
-    def authorize_url
-      ::Instagram.authorize_url(:redirect_uri => redirect_uri)
-    end
-
-    def get_access_token(code)
-      ::Instagram.get_access_token(code, :redirect_uri => redirect_uri).access_token
-    end
-
-  end
+  require_relative './auth/instagram_connector'
 
   def connect
     redirect_to InstagramConnector.new(auth_callback_url).authorize_url
