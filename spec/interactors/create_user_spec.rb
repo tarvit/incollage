9f4d3883c5876f6c220d0066f52f6101c01ab309
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-09-25 09:15:09 +0300

    add more test cases

--- spec/interactors/create_user_spec.rb
+++ spec/interactors/create_user_spec.rb
@@ -15,13 +15,13 @@ describe Incollage::UserCreator do
     expect(Incollage::Repository.for_user.count).to eq(0)
 
     Incollage::UserCreator.new({full_name: 'Johny D'}).find_or_create
-    user = Incollage::Repository.for_user.find(full_name: 'Johny D')
-
     expect(Incollage::Repository.for_user.count).to eq(1)
 
     Incollage::UserCreator.new({full_name: 'Johny D'}).find_or_create
-
     expect(Incollage::Repository.for_user.count).to eq(1)
+
+    Incollage::UserCreator.new({full_name: 'Billy K'}).find_or_create
+    expect(Incollage::Repository.for_user.count).to eq(2)
   end
 
 end
