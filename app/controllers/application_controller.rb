96283b616fe3af98c09eb00cf1b5ec38413d87ae
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-10-09 19:16:10 +0300

    set adapters for rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,11 +25,18 @@ class ApplicationController < ActionController::Base
 
   def self.reload_core
     Incollage.load_modules(Rails.root.join('app/core'))
+    set_adapters
   end
 
   def reload_core
     self.class.reload_core
   end
 
+  def self.set_adapters
+    Incollage::Repository.register(:user, UserActiveRecord::Repository.new)
+    Incollage::Repository.register(:clipping, ClippingActiveRecord::Repository.new)
+  end
+
   reload_core
+  set_adapters
 end
--- app/controllers/collage_controller.rb
+++ app/controllers/collage_controller.rb
@@ -2,8 +2,20 @@ class CollageController < ApplicationController
   before_filter :check_authorized!
 
   def builder
-    @client = ::Instagram.client(:access_token => access_token)
-    @images = @client.user_media_feed.map{|item| item.images.low_resolution.url}
+    @current_user = execute_user
+    @images = instagram_client.user_media_feed.map{|item| item.images.low_resolution.url}
+  end
+
+  private
+
+  def instagram_client
+    @client ||= ::Instagram.client(:access_token => access_token)
+  end
+
+  def execute_user
+    user = instagram_client.user
+    data = { full_name: user.full_name, username: user.username }
+    Incollage::FindOrCreateUser.new(data).execute
   end
 
 end
