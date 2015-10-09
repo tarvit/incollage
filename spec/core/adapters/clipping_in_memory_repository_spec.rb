0d7ec01dcd5c32f6bbc7a27cf543dff3e383c0fe
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-10-09 20:24:03 +0300

    -/-

--- spec/core/adapters/clipping_in_memory_repository_spec.rb
+++ spec/core/adapters/clipping_in_memory_repository_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe Incollage::Repository::ClippingInMemoryRepository do
 
+  require app_root.join 'spec/core/support/clipping_repository_test'
+  include ClippingRepositoryTest
+
   before :each do
     @repo = Incollage::Repository::InMemoryBase.new
   end
-
-  require app_root.join 'spec/core/support/clipping_repository_test'
-  include ClippingRepositoryTest
 
   test_clipping_repository
 
