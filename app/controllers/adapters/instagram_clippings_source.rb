d3e2055684a6aa6665b8bb68a6cd5cca35671560
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-10-11 19:52:36 +0300

    adapt instagram source to new synchronizations

--- app/controllers/adapters/instagram_clippings_source.rb
+++ app/controllers/adapters/instagram_clippings_source.rb
@@ -1,82 +1,84 @@
 class InstagramClippingsSource
 
-  def initialize(instagram_client)
-    @client = instagram_client
-  end
+  class Base
 
-  def next_clippings(collection, last_clipping)
-    feed = last_clipping.nil? ? whole_collection_items : new_items(last_clipping.external_id)
-
-    entities = feed.map do |item|
-      InstagramMediaClipping.new(item, collection).to_entity
+    def initialize(instagram_client)
+      @client = instagram_client
     end
 
-    entities.sort_by{|x| x[:external_created_time] }
-  end
-
-  def new_items(external_id)
-    @client.user_media_feed( min_id: external_id )
-  end
-
-  def whole_collection_items
-    items = []
-    current_page = @client.user_media_feed
-    items << current_page
-    next_max_id = current_page.pagination.next_max_id
-
-    while next_max_id
-      current_page = @client.user_media_feed(max_id: next_max_id)
-      items << current_page
-      next_max_id = current_page.pagination.next_max_id
-    end
-
-    items.flatten(1)
-  end
-
-  class InstagramMediaClipping
-    require 'open-uri'
-    attr_reader :media_item, :collection
-
-    def initialize(media_item, collection)
-      @media_item = media_item
-      @collection = collection
-    end
-
-    def to_entity
-      path = make_picture_url
-      {
-          user_id: collection.user_id,
-          external_id: media_item.id,
-          external_created_time: media_item.created_time,
-          collection_id: collection.id,
-          picture_url: path,
-          histogram: make_histogram(path),
-      }
-    end
-
-    protected
-
-    def make_histogram(url)
-      puts url
-      file = save_to_tempfile(url, "instagram_media_#{media_item.id}")
-      Incollage::HistogramMaker.new(file.path).make
-    end
-
-    def make_picture_url
-      media_item.images.low_resolution.url
-    end
-
-    def save_to_tempfile(url, filename)
-      uri = URI.parse(url.gsub('https://', 'http://'))
-      Net::HTTP.start(uri.host, uri.port) do |http|
-        resp = http.get(uri.path)
-        file = Tempfile.new(filename, Dir.tmpdir, 'wb+')
-        file.binmode
-        file.write(resp.body)
-        file.flush
-        file
+    def next_clippings(collection, last_clipping)
+      new_items(last_clipping.try(:external_id)).map do |item|
+        InstagramMediaClipping.new(item, collection).to_entity
       end
     end
+
+    def new_items(external_id)
+      raise NotImplementedError
+    end
+
+    class InstagramMediaClipping
+      require 'open-uri'
+      attr_reader :media_item, :collection
+
+      def initialize(media_item, collection)
+        @media_item = media_item
+        @collection = collection
+      end
+
+      def to_entity
+        path = make_picture_url
+        {
+            user_id: collection.user_id,
+            external_id: media_item.id,
+            external_created_time: media_item.created_time,
+            collection_id: collection.id,
+            picture_url: path,
+            histogram: make_histogram(path),
+        }
+      end
+
+      protected
+
+      def make_histogram(url)
+        puts url
+        file = save_to_tempfile(url, "instagram_media_#{media_item.id}")
+        Incollage::HistogramMaker.new(file.path).make
+      end
+
+      def make_picture_url
+        media_item.images.low_resolution.url
+      end
+
+      def save_to_tempfile(url, filename)
+        uri = URI.parse(url.gsub('https://', 'http://'))
+        Net::HTTP.start(uri.host, uri.port) do |http|
+          resp = http.get(uri.path)
+          file = Tempfile.new(filename, Dir.tmpdir, 'wb+')
+          file.binmode
+          file.write(resp.body)
+          file.flush
+          file
+        end
+      end
+    end
+
   end
+
+  class Recent < Base
+    def new_items(external_id)
+      @client.user_media_feed( min_id: external_id )
+    end
+
+    alias_method :recent_clippings, :next_clippings
+  end
+
+  class Preceding < Base
+    def new_items(external_id)
+      @client.user_media_feed(max_id: external_id)
+    end
+
+    alias_method :preceding_clippings, :next_clippings
+  end
+
 
 end
--- app/controllers/clippings_controller.rb
+++ app/controllers/clippings_controller.rb
@@ -2,8 +2,13 @@ class ClippingsController < ApplicationController
 
   def synchronize
     collection = Incollage::ClippingsCollection.new(current_user.id, 1)
-    instagram_source = InstagramClippingsSource.new(instagram_client)
-    Incollage::SynchronizeClippings.new(collection, instagram_source).sync
+
+    instagram_source = InstagramClippingsSource::Recent.new(instagram_client)
+    Incollage::SynchronizeRecentClippings.new(collection, instagram_source).execute
+
+    # instagram_source = InstagramClippingsSource::Preceding.new(instagram_client)
+    # Incollage::SynchronizePrecedingClippings.new(collection, instagram_source).execute
+
     redirect_to collage_builder_path
   end
 
@@ -11,7 +16,7 @@ class ClippingsController < ApplicationController
     collection = Incollage::ClippingsCollection.new(current_user.id, 1)
     colors = params[:colors].split(?,)
     clippings = Incollage::SearchClippingsForCollage.new(collection, colors, 5).execute
-    @images = clippings.map &:picture_url
+    @images = clippings.sort_by(&:external_id)
     @collage = @images.first
 
     render 'collage/builder'
