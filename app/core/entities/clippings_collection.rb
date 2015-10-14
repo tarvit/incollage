a2f9656b24586e5c50004c183386ac5105918e5a
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-10-14 13:53:45 +0300

    CL: remove needless attributes from clippings collection entity

--- app/core/entities/clippings_collection.rb
+++ app/core/entities/clippings_collection.rb
@@ -1,9 +1,9 @@
 class Incollage::ClippingsCollection < Incollage::Entity::Base
 
-  attr_accessor :user_id, :id, :clippings
+  attr_accessor :user_id, :id
 
   def initialize(user_id, id)
-    @user_id, @id, @clippings = user_id, id
+    @user_id, @id = user_id, id
   end
 
 end
