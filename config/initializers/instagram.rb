52956e4bdc9281e8b8055e04c754e70290da145e
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-10-15 11:33:37 +0300

    CONF: remove hardcode form configrutation

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,4 +1,4 @@
 Instagram.configure do |config|
-  config.client_id = '7e70694d8f734df18b57fb1eaeaf7c90'
-  config.client_secret = '87fada234dfc4be4a47e3890059a8c08'
+  config.client_id = Rails.application.secrets.instagram_client_id
+  config.client_secret = Rails.application.secrets.instagram_secret
 end
