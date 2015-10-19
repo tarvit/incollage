65d4c8b9b965fde521bd83ed71d85668249ea78a
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-10-19 19:39:45 +0300

    TEST: update test due to the latest interface requirements

--- spec/core/interactors/authorize_user_spec.rb
+++ spec/core/interactors/authorize_user_spec.rb
@@ -4,37 +4,37 @@ describe Incollage::AuthenticateUser do
 
   before :each do
     @user = TestFactories::UserFactory.create(username: 'coolname', password: 'coolpass_123')
-    @session = TestSupport::FakeAbstractService.new(:authenticate)
+    @session = TestSupport::FakeAbstractService.new(:certify_user)
   end
 
   it 'should authorize a user with valid creds' do
-    expect(@session).to receive(:authenticate).with(@user.id)
+    expect(@session).to receive(:certify_user).with(@user.id)
     Incollage::AuthenticateUser.new(@session, username: 'coolname', password: 'coolpass_123').execute
   end
 
   it 'should raise error if user doesn\'t exists' do
-    expect(@session).to_not receive(:authenticate)
+    expect(@session).to_not receive(:certify_user)
     expect(->{
       Incollage::AuthenticateUser.new(@session, username: 'coolname2', password: 'awsomepass22').execute
     }).to raise_error(Incollage::AuthenticateUser::AuthenticationFailedError)
   end
 
   it 'should raise error if username is not passed' do
-    expect(@session).to_not receive(:authenticate)
+    expect(@session).to_not receive(:certify_user)
     expect(->{
       Incollage::AuthenticateUser.new(@session, password: 'awsomepass22').execute
     }).to raise_error(Incollage::AuthenticateUser::AuthenticationFailedError)
   end
 
   it 'should raise error if password is not passed' do
-    expect(@session).to_not receive(:authenticate)
+    expect(@session).to_not receive(:certify_user)
     expect(->{
       Incollage::AuthenticateUser.new(@session, password: 'coolpass_123').execute
     }).to raise_error(Incollage::AuthenticateUser::AuthenticationFailedError)
   end
 
   it 'should raise error if password is invalid' do
-    expect(@session).to_not receive(:authenticate)
+    expect(@session).to_not receive(:certify_user)
     expect(->{
       Incollage::AuthenticateUser.new(@session, username: 'coolname', password: 'invalid_password').execute
     }).to raise_error(Incollage::AuthenticateUser::AuthenticationFailedError)
