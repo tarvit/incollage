7b8076c815634bde56c8ba27c74515219a7dc053
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-10-22 13:54:22 +0300

    UI: display sidebar info

--- app/controllers/collage_controller.rb
+++ app/controllers/collage_controller.rb
@@ -2,7 +2,9 @@ class CollageController < ApplicationController
   before_filter :check_authorized!
 
   def builder
-    @clippings= []
+    stats = Incollage::GetUserAccountsStatistics.new(current_user.id).execute
+    pp stats
+    @accounts_info = TarvitHelpers::HashPresenter.present(stats)
     # @page = params[:page].to_i
     # #clippings = Incollage::FindClippingsPage.new(current_user.id, DEFAULT_COLLECTION_ID, @page, PER_PAGE).execute
     # clippings = Incollage::ClippingsFinder.new(current_user_collection).find_all
