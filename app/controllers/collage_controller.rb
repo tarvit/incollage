48ea09e1bb224fe4ee7cbadeaa8d504ea934d993
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-10-23 13:00:21 +0300

    display collage

--- app/controllers/collage_controller.rb
+++ app/controllers/collage_controller.rb
@@ -3,21 +3,35 @@ class CollageController < ApplicationController
   before_filter :init_current_stats
 
   def builder
-    @clippings = all_clippings
+    if params[:create_collage]
+      create_collage
+    else
+      @clippings = all_clippings
+    end
   end
 
   protected
 
+  def create_collage
+    @colors = params[:colors].split(?,)
+    @clippings = Incollage::SearchClippingsForCollage.new({ user_id: current_user.id }, @colors, 4).execute
+
+    collage_file = Incollage::MakeCollageFromClippings.new(@clippings[0..3], current_user.id).execute
+    @collage = collage_url(collage_file)
+  end
+
   def all_clippings
     options = {
         user_id: current_user.id,
-        per_page: 10,
+        per_page: 9999,
         page_number: 0,
     }
 
-    Incollage::FindClippingsPage.new(options).execute
-        .sort_by(&:external_created_time)
-        .uniq(&:external_id).reverse
+    Incollage::FindClippingsPage.new(options).execute.uniq(&:external_id)
+  end
+
+  def collage_url(file)
+    file.path.gsub(Rails.root.join('public').to_s,'')
   end
 
 end
--- app/core/interactors/search_clippings_for_collage.rb
+++ app/core/interactors/search_clippings_for_collage.rb
@@ -1,12 +1,12 @@
 module Incollage
   class SearchClippingsForCollage
 
-    def initialize(clipping_collection, palette_colors, count)
-      @clipping_collection, @palette_colors, @count = clipping_collection, palette_colors, count
+    def initialize(options, palette_colors, count)
+      @options, @palette_colors, @count = options, palette_colors, count
     end
 
     def execute
-      all = ClippingsFinder.new(@clipping_collection).find_all
+      all = ClippingsFinder.new(@options).find_all
       all.sort_by{|cl| -Service.for_color_matcher.score(cl.histogram.scores, @palette_colors) }[0..(@count-1)]
     end
 
