5a0db7964d43d094f2c94f3ebd05b80125502e1b
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2015-11-12 19:06:54 +0200

    TEST: add flick media clipping test

---
+++ spec/adapters/flickr/flick_media_clipping_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe FlickrAdapter::MediaClipping do
+
+  before :each do
+    @collection = Incollage::UserClippingsCollection.new(user_id: 1, collection_id: 2, linked_account_id: 1)
+    @media_item = fake_media_item
+  end
+
+  it 'should transform response to clipping entity' do
+    @media_clipping = FlickrAdapter::MediaClipping.new(@media_item, @collection)
+    entity_attrs = @media_clipping.to_entity_attrs
+    expect(entity_attrs).to eq({
+      user_id: 1,
+      collection_id: 2,
+      linked_account_id: 1,
+      external_id: 4,
+      picture: {
+          url: 'media_url',
+          external_created_time: 22,
+          histogram: { 1 => '00ff00' },
+      },
+    })
+    entity = Incollage::Clipping.new(entity_attrs)
+    expect(entity.valid?).to be_truthy
+  end
+
+  def fake_media_item
+    TarvitHelpers::HashPresenter.present({
+        id: 4,
+        owner: '252525@N55',
+        secret: '0001111',
+        server: 577,
+        farm: 1,
+        title: 'images',
+        ispublic: 1,
+        isfriend: 0,
+        isfamily: 0,
+        dateupload: 22,
+        url_m: 'media_url',
+        height_m: 150,
+        width_m: 270,
+     })
+  end
+
+end
